c183067ee3f7d58d8ee4993a7d9776d5fa10a90d
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-27 17:03:30 +0530

    three strings to find maximum program

--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -6,17 +6,21 @@
         {
             Console.WriteLine("Welcome to find maximum using generics program");
             FindMaximum findMaximum = new FindMaximum();
-            Console.WriteLine("\n1.Find Max Of Three Int Number\n2.Find Max Of Three Float Number");
+            Console.WriteLine("\n1.Find Max Of Three Int Number\n2.Find Max Of Three Float Number\n3.Find Max Of Three Strings");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
-                case 1:            
-            int max = findMaximum.MaxOfThree(09, 18, 27);
-            Console.WriteLine("Maximum is " + max);
+                    case 1:            
+                    int max = findMaximum.MaxOfThree(09, 18, 27);
+                    Console.WriteLine("Maximum is " + max);
                     break;
                     case 2:
-            float fmax = findMaximum.MaxOfThreeFloat(18.27f, 45.63f, 72.99f);
-            Console.WriteLine("Maximum is " + fmax);
+                    float fmax = findMaximum.MaxOfThreeFloat(18.27f, 45.63f, 72.99f);
+                    Console.WriteLine("Maximum is " + fmax);
+                    break;
+                    case 3:
+                    string smax = findMaximum.MaxOfThreeString("abc", "pqr", "xyz");
+                    Console.WriteLine("Maximum is "+  smax);
                     break;
             }
         }
