dae0875f32c773390176e6ae5f9349ff545e1d52
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-27 16:33:46 +0530

    Adding three integers to find maximum program

---
+++ FindMaximumUsingGenerics/FindMaximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumUsingGenerics
+{
+    internal class FindMaximum
+    {
+        public int MaxOfThree(int a, int b, int c)
+        {
+            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+            else if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+            else if (c.CompareTo(a) >= 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+            }
+            return default;
+        }
+    }
+}
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to find maximum using generics program");
+
+            FindMaximum findMaximum = new FindMaximum();
+            int max = findMaximum.MaxOfThree(09, 18, 27);
+            Console.WriteLine("Maximum is "+ max);
         }
     }
 }
