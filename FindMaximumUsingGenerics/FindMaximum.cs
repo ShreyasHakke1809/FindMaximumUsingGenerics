a9b6f57818857e3fbbfaf148ee6b332b54e30dcb
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-27 17:21:31 +0530

    Extent max method to take more parameters

--- FindMaximumUsingGenerics/FindMaximum.cs
+++ FindMaximumUsingGenerics/FindMaximum.cs
@@ -6,55 +6,33 @@ using System.Threading.Tasks;
 
 namespace FindMaximumUsingGenerics
 {
-    internal class FindMaximum
+    internal class FindMaximum <T> where T : IComparable
     {
-        public int MaxOfThree(int a, int b, int c)
+        public T[] genValue;
+
+        public FindMaximum(T[] genValue)                      
         {
-            if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
-            {
-                return a;
-            }
-            else if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
-            {
-                return b;
-            }
-            else if (c.CompareTo(a) >= 0 && c.CompareTo(b) >= 0)
-            {
-                return c;
-            }
-            return default;
+            this.genValue = genValue;
         }
-        public float MaxOfThreeFloat (float num1, float num2, float num3)
+
+        public T[] Sorting(T[] genValue)
         {
-            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
-            {
-                return num1;
-            }
-            else if (num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) >= 0)
-            {
-                return num2;
-            }
-            else if (num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) >= 0)
-            {
-                return num3;
-            }
-            return default;
+            Array.Sort(genValue);
+            Array.Reverse(genValue);
+            return genValue;
         }
-        public string MaxOfThreeString(string str1, string str2, string str3)
+
+        public T GetMaxOfThree(T[] genValue)
         {
-            if (str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) >= 0)
-            {
-                return str1;
-            }
-            else if (str2.CompareTo(str1) >= 0 && str2.CompareTo(str3) >= 0)
-            {
-                return str2;
-            }
-            else if (str3.CompareTo(str1) >= 0 && str3.CompareTo(str2) >= 0)
-            {
-                return str3;
-            }
-            return default;
+            var sortedArray = Sorting(genValue);
+            return sortedArray.ElementAt(0);
         }
+        public T GetMax()
+        {
+            T maxValue = GetMaxOfThree(this.genValue);
+            return maxValue;
+        }
+
     }
 }
+
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -5,21 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to find maximum using generics program");
-            FindMaximum findMaximum = new FindMaximum();
             Console.WriteLine("\n1.Find Max Of Three Int Number\n2.Find Max Of Three Float Number\n3.Find Max Of Three Strings");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
-                    case 1:            
-                    int max = findMaximum.MaxOfThree(09, 18, 27);
+                    case 1:
+                    int[] intArray = {09,18,27,36};
+                    int max = new FindMaximum<int>(intArray).GetMax();
                     Console.WriteLine("Maximum is " + max);
                     break;
                     case 2:
-                    float fmax = findMaximum.MaxOfThreeFloat(18.27f, 45.63f, 72.99f);
+                    float[] floatArray = {09.18f, 27.09f, 45.63f, 72.99f};
+                    float fmax = new FindMaximum<float>(floatArray).GetMax();
                     Console.WriteLine("Maximum is " + fmax);
                     break;
                     case 3:
-                    string smax = findMaximum.MaxOfThreeString("abc", "pqr", "xyz");
+                    string[] strArray = {"abc", "lmn", "pqr", "xyz"};
+                    string smax = new FindMaximum<string>(strArray).GetMax();
                     Console.WriteLine("Maximum is "+  smax);
                     break;
             }
