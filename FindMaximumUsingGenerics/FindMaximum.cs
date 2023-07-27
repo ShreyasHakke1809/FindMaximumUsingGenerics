b17dfba6c4e24f9c34d9c13683dbba4332165ad8
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-27 16:57:11 +0530

    three floats to find maximum program

--- FindMaximumUsingGenerics/FindMaximum.cs
+++ FindMaximumUsingGenerics/FindMaximum.cs
@@ -24,5 +24,21 @@ namespace FindMaximumUsingGenerics
             }
             return default;
         }
+        public float MaxOfThreeFloat (float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            else if (num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return default;
+        }
     }
 }
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -5,10 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to find maximum using generics program");
-
             FindMaximum findMaximum = new FindMaximum();
+            Console.WriteLine("\n1.Find Max Of Three Int Number\n2.Find Max Of Three Float Number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:            
             int max = findMaximum.MaxOfThree(09, 18, 27);
-            Console.WriteLine("Maximum is "+ max);
+            Console.WriteLine("Maximum is " + max);
+                    break;
+                    case 2:
+            float fmax = findMaximum.MaxOfThreeFloat(18.27f, 45.63f, 72.99f);
+            Console.WriteLine("Maximum is " + fmax);
+                    break;
+            }
         }
     }
 }
