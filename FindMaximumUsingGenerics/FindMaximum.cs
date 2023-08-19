ffebd4f7b2dc2e9afe0bbfb432b3da1115131860
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-08-19 18:00:25 +0530

    Refactor two using constructor

--- FindMaximumUsingGenerics/FindMaximum.cs
+++ FindMaximumUsingGenerics/FindMaximum.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FindMaximumUsingGenerics
+﻿namespace FindMaximumUsingGenerics
 {
-    internal class FindMaximum 
+    public class FindMaximum<T> where T : IComparable
     {
-        public T GetMaxOfThree<T>(T a, T b, T c) where T : IComparable
+        public T num1, num2, num3;
+
+        public FindMaximum(T num1, T num2, T num3)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+        public static T GetMaxOfThree<T>(T a, T b, T c) where T : IComparable
         {
             if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
             {
@@ -24,8 +26,14 @@ namespace FindMaximumUsingGenerics
             }
             return default;
         }
+        public T GetMax()
+        {
+            T maxValue = FindMaximum<T>.GetMaxOfThree(this.num1, this.num2, this.num3);
+            return maxValue;
+        }
+
+
 
     }
 }
-
 
--- FindMaximumUsingGenerics/Program.cs
+++ FindMaximumUsingGenerics/Program.cs
@@ -5,25 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to find maximum using generics program");
-            FindMaximum findMaximum = new FindMaximum();
+            //FindMaximum findMaximum = new FindMaximum();
             Console.WriteLine("\n1.Find Max Of Three Int Number\n2.Find Max Of Three Float Number\n3.Find Max Of Three Strings");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
-                    case 1:
-                    int a = 09; int b = 18; int c = 27; 
-                    int max = findMaximum.GetMaxOfThree(a,b,c);
-                    Console.WriteLine("Maximum out of {0}, {1}, {2} integers is: {3}", a,b,c,max);  
+                case 1:
+                    int a = 09; int b = 18; int c = 27;
+                    int max = new FindMaximum<int>(a, b, c).GetMax();
+                    Console.WriteLine("Maximum out of {0}, {1}, {2} integers is: {3}", a, b, c, max);
                     break;
-                    case 2:
+                case 2:
                     float p = 27.09f; float q = 45.18f; float r = 72.99f;
-                    float fmax = findMaximum.GetMaxOfThree(p,q,r);
-                    Console.WriteLine("Maximum out of {0}, {1}, {2} float is: {3}", p,q,r, fmax);
+                    float fmax = new FindMaximum<float>(p, q, r).GetMax();
+                    Console.WriteLine("Maximum out of {0}, {1}, {2} float is: {3}", p, q, r, fmax);
                     break;
-                    case 3:
+                case 3:
                     string x = "Xyz"; string y = "Abc"; string z = "Pqr";
-                    string smax = findMaximum.GetMaxOfThree(x,y,z);
-                    Console.WriteLine("Maximum out of {0}, {1}, {2} string is: {3}", x,y,z,smax);
+                    string smax = new FindMaximum<string>(x, y, z).GetMax();
+                    Console.WriteLine("Maximum out of {0}, {1}, {2} string is: {3}", x, y, z, smax);
                     break;
             }
         }
